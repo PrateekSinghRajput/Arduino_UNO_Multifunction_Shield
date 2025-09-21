8ce010ed1ef6fb195a835cafe51feb9d1cbf52cf
Prateek Singh <92904643+PrateekSinghRajput@users.noreply.github.com>
2025-09-21 23:40:34 +0530

    Update Potentiometer.ino

--- 13_Potentiometer/Potentiometer.ino
+++ 13_Potentiometer/Potentiometer.ino
@@ -1,15 +1,32 @@
-const int red=11;
+int sensorPin = A0;    
+int digitalValue = 0;  
 
 void setup() {
-
   Serial.begin(9600);
-  pinMode(red,OUTPUT);
 }
 
 void loop() {
+  digitalValue = analogRead(sensorPin);
+  Serial.print("digital value = ");
+  Serial.println(digitalValue);
+  delay(1000);
+}
 
-  int poten=analogRead(A0);
-  Serial.println(poten);
-  analogWrite(red,poten);
-  
+// ADC With Voltage
+int sensorPin = A0;    
+int digitalValue = 0;  
+float analogVoltage = 0.00;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalValue = analogRead(sensorPin);
+  Serial.print("digital value = ");
+  Serial.print(digitalValue);
+  analogVoltage = (digitalValue * 5.00) / 1023.00;
+  Serial.print("  analog voltage = ");
+  Serial.println(analogVoltage);
+  delay(1000);
 }
